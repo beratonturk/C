ef00815e58d5e7678c18d71173e1de9c78847b4a
Berat Öntürk <berat.onturk.bo@icloud.com>
2023-01-03 23:27:03 +0300

    added

---
+++ name, date of birth and mobile number.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Name   : Berat Öntürk");
+
+    printf("\nDOB    : january 11, 1989"); // The date of birth is given for purely illustrative purposes and does not belong to any individual.
+
+    printf("\nMobile : (999) 999-9999"); // The phone number is provided purely for illustrative purposes and does not belong to any individual.
+
+    return (0);
+}
