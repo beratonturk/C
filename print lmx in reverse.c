e1458abb5ff6d04bafefbd3a4c0a389fba421c05
Berat Öntürk <berat.onturk.bo@icloud.com>
2023-01-06 23:52:00 +0300

    added

---
+++ print lmx in reverse.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char x = 'X';
+    char m = 'M';
+    char l = 'L';
+
+    printf("First : %c%c%c", x, m, l);
+
+    printf("\nThen :%c%c%c", l, m, x);
+
+    return (0);
+}
