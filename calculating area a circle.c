75b74f62b44dad202532eb441a848005681b12fc
Berat Öntürk <berat.onturk.bo@icloud.com>
2023-01-09 21:10:48 +0300

    added

---
+++ calculating area a circle.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int radius;
+    float area, perimeter;
+    radius = 6;
+
+    perimeter = 2 * 3.14 * radius;
+    printf("Perimeter of the Circle = %f inches\n", perimeter);
+
+    area = 3.14 * radius * radius;
+    printf("Area of the Circle = %f square inches", area);
+
+    return (0);
+}
