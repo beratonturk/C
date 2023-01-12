82bfaa904f6e34ee87de29926402cb41c6e57187
Berat Öntürk <berat.onturk.bo@icloud.com>
2023-01-12 22:25:24 +0300

    added

---
+++ calculate the product of the entered.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y, result;
+
+    printf("\nInput the first integer: ");
+    scanf("%d", &x);
+
+    printf("\nInput the second integer: ");
+    scanf("%d", &y);
+
+    result = x * y;
+
+    printf("Product of the above two integers = %d\n", result);
+
+    return (0);
+}
