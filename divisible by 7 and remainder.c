8bf5d9c876c6aca2164d0552a236186c23ad7a15
Berat Öntürk <berat.onturk.bo@icloud.com>
2023-02-23 01:20:21 +0300

    added

---
+++ divisible by 7 and remainder.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y, temp, i, sum = 0;
+
+    printf("\nInput the first integer: ");
+    scanf("%d", &x);
+
+    printf("\nInput the second integer: ");
+    scanf("%d", &y);
+
+    if (x > y)
+    {
+        temp = y;
+        y = x;
+        x = temp;
+    }
+
+    for (i = x; i < y; i++)
+    {
+        if ((i % 7) == 2 || (i % 7) == 3)
+        {
+            printf("%d\n", i);
+        }
+    }
+
+    return (0);
+}
