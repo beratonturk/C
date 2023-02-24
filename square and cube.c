73df9dcfeefc02d0800bf51823b02a33afe530a8
Berat Öntürk <berat.onturk.bo@icloud.com>
2023-02-24 23:30:58 +0300

    added

---
+++ square and cube.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    int a, i;
+
+    printf("Input number of lines: ");
+    scanf("%d", &a);
+
+    for (i = 1; i <= a; i++)
+    {
+        printf("%d %d %d\n", i, i * i, i * i * i);
+    }
+
+    return 0;
+}
