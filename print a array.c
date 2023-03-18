c8919933b737438066cc75a17f158fef88360210
Berat Öntürk <berat.onturk.bo@icloud.com>
2023-03-19 01:48:20 +0300

    added

---
+++ print a array.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main()
+{
+    int n[5], i, x;
+
+    printf("Input the 5 members of the array:\n");
+
+    for (i = 0; i < 5; i++)
+    {
+        scanf("%d", &x);
+
+        if (x > 0)
+        {
+            n[i] = x;
+        }
+
+        else
+        {
+            n[i] = 100;
+        }
+    }
+
+    printf("\nArray values are: \n");
+
+    for (i = 0; i < 5; i++)
+    {
+        printf("n[%d] = %d\n", i, n[i]);
+    }
+
+    return 0;
+}
