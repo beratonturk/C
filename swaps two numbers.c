707a7b4076cd35fc0a39769c79806a8feca435f7
Berat Öntürk <berat.onturk.bo@icloud.com>
2023-07-18 13:55:21 -0800

    added

---
+++ swaps two numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int x, y;
+
+    printf("Input value for x & y: \n");
+    scanf("%d%d", &x, &y);
+
+    printf("Before swapping the value of x & y: %d %d", x, y);
+
+    x = x + y;
+    y = x - y;
+    x = x - y;
+
+    printf("\nAfter swapping the value of x & y: %d %d\n", x, y);
+
+    return (0);
+}
