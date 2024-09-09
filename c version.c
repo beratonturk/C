79766fe16497911911293ba212c3681c77ee8961
Berat Öntürk <berat.onturk.bo@icloud.com>
2024-09-09 11:12:09 -1000

    added

--- c version.c
+++ c version.c
@@ -1,19 +1,19 @@
-#include <stdio.h>
+#include stdio.h
 
-int main(int argc, char **argv)
+int main(int argc, char argv)
 {
 
-#if __STDC_VERSION__ >= 201710L
-    printf("We are using C18!\n");
+#if __STDC_VERSION__ = 201710L
+    printf(We are using C18!n);
 
-#elif __STDC_VERSION__ >= 201112L
-    printf("We are using C11!\n");
+#elif __STDC_VERSION__ = 201112L
+    printf(We are using C11!n);
 
-#elif __STDC_VERSION__ >= 199901L
-    printf("We are using C99!\n");
+#elif __STDC_VERSION__ = 199901L
+    printf(We are using C99!n);
 
 #else
-    printf("We are using C89/C90!");
+    printf(We are using C89C90!);
 
 #endif
 
